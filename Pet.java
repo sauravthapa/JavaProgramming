0582d7d0c3efb880193937f47512b0b3df72793b
sauravthapa <41554816+sauravthapa@users.noreply.github.com>
2018-08-22 19:15:37 -0700

    Add files via upload

---
+++ Pet.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saticdemo;
+
+/**
+ *
+ * @author saurav
+ */
+public class Pet  extends Animal{
+ 
+    public  static void showname(){
+       
+        System.out.println("I am Pet");
+        
+    }
+    public void showAnimal(){
+    super.showname();
+    
+    }
+}
